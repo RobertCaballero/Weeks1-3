243293e02003a49dd2abb0163f5d01e7db6f23ac
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-01-16 17:05:34 -0500

    Week 2 inclass demo Hider Script Mover script Updates Pulser Missile Tank

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -19,7 +19,9 @@ public class Mover : MonoBehaviour
     void Update()
     {
         Vector3 moverXPos = transform.position;
-        moverXPos.x = moverXPos.x + speed;
+        moverXPos.x += speed * Time.deltaTime;
+
+
         transform.position = moverXPos;
 
 
